b495f90858e1996701b8f8cb8a8ba3c33346e1e1
MccreeFei <mccreefei@foxmail.com>
2019-08-05 09:46:42 +0800

    add leetcode solution

---
+++ leetcode/src/main/java/cn/mccreefei/technologystack/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,39 @@
+package cn.mccreefei.technologystack.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author MccreeFei
+ * @create 2019-08-05 上午9:38
+ * <href>https://leetcode.com/problems/binary-tree-inorder-traversal/</href>
+ * @idea 二叉树的中序遍历
+ */
+public class BinaryTreeInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        inOrder(root, result);
+        return result;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            inOrder(root.left, res);
+        }
+        res.add(root.val);
+        inOrder(root.right, res);
+    }
+}
