27ec6373870bb77e62f2b394a4583859fe04e612
MccreeFei <mccreefei@foxmail.com>
2020-03-23 10:05:31 +0800

    add leetcode solution

---
+++ leetcode/src/main/java/cn/mccreefei/technologystack/leetcode/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,42 @@
+package cn.mccreefei.technologystack.leetcode;
+
+/**
+ * @author MccreeFei
+ * @create 2020-03-23 上午10:03
+ * @refer <href>https://leetcode.com/problems/populating-next-right-pointers-in-each-node/</href>
+ * @idea 递归
+ */
+public class PopulatingNextRightPointersInEachNode {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        root.left.next = root.right;
+        if (root.next != null) {
+            root.right.next = root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+}
